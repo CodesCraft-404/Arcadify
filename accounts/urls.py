8a8e981f3de5cacf5e05e621e1a8a5a279fe7b69
CodesCraft-404 <chalkeaditya310@gmail.com>
2026-03-12 19:09:23 +0530

    disable Django admin

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
     path('', views.login_register_view, name='login_register'),
     path('home/', views.home_view, name='home'),
     path('logout/', views.logout_view, name='logout'),
