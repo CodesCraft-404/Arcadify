02d11227e30cedb3a3a74483f1ad6e36ea109348
CodesCraft-404 <chalkeaditya310@gmail.com>
2026-03-13 18:54:28 +0530

    remove

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,19 +1,6 @@
 from django.apps import AppConfig
 
+
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-        from django.contrib.auth import get_user_model
-
-        User = get_user_model()
-
-        if not User.objects.filter(gmail="admin@gmail.com").exists():
-            User.objects.create_superuser(
-                gmail="admin@gmail.com",
-                name="Admin",
-                gamer_name="admin",
-                age=21,
-                password="admin123"
-            )
--- accounts/views.py
+++ accounts/views.py
@@ -76,16 +76,3 @@ def admin_page_view(request):
         return redirect('home')
 
     return render(request, 'accounts/admin.html')
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(gmail="admin@gmail.com").exists():
-    User.objects.create_superuser(
-        gmail="admin@gmail.com",
-        name="Admin",
-        gamer_name="admin",
-        age=21,
-        password="admin123"
-    )
