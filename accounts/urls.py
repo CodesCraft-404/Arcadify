a69cb6f244a0c1d8e5f705298044681bb2c8a82f
CodesCraft-404 <chalkeaditya310@gmail.com>
2026-03-16 01:14:21 +0530

    remove Friend system

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,11 +7,4 @@ urlpatterns = [
     path('home/', views.home_view, name='home'),
     path('logout/', views.logout_view, name='logout'),
     path('admin-panel/', views.admin_page_view, name='admin_page'),
-
-    # Friends system
-    path('friends/', views.friends_list_view, name='friends_list'),
-    path('friends/ajax/', views.get_friends_ajax, name='get_friends_ajax'),
-    path('friends/send/', views.send_friend_request_ajax, name='send_friend_request_ajax'),
-    path('friends/respond/', views.respond_friend_request_ajax, name='respond_friend_request_ajax'),
-    path('friends/search/', views.search_users_ajax, name='search_users_ajax'),
 ]
